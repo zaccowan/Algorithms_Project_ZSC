8933a473e88ab50c9480294886d46097d6017492
Zac Cowan <zcowan@bellarmine.edu>
2023-12-04 20:45:36 -0500

    Update SelectionSort to match pseudocode

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -7,26 +7,23 @@ public class SelectionSort {
         int n;
         n = unsortedArr.length;
 
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n-2; i++) {
             // Assume the min element is at i
-            int min_idx = i;
+            int minIndex = i;
 
             // Compare all elements after i to determine the minimum
             for (int j = i + 1; j < n; j++) {
-                if (unsortedArr[j] < unsortedArr[min_idx]) {
-                    min_idx = j;
+                if (unsortedArr[j] < unsortedArr[minIndex]) {
+                    minIndex = j;
                 }
             }
 
             // Swap i with the minimum found
-            int temp = unsortedArr[min_idx];
-            unsortedArr[min_idx] = unsortedArr[i];
-            unsortedArr[i] = temp;
+            int temp = unsortedArr[minIndex];
+            unsortedArr[i] = unsortedArr[minIndex];
+            unsortedArr[minIndex] = temp;
         }
 
-//        if(doPrint) {
-//            printArray(unsortedArr);
-//        }
     }
 
     public void printArray(int[] arr) {
